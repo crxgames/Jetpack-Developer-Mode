c84a0f5d287ba5302ab55ac36f14d49640f14965
U-CNGNET\cmays <cmays@cmays-ni.cngfinancial.com>
2014-06-23 16:45:16 -0400

    Check to verify jetpack is loaded

--- jetpack-developer.php
+++ jetpack-developer.php
@@ -3,7 +3,13 @@
  * Plugin Name: JetPack Development Mode
  * Plugin URI:  http://wordpress.stackexchange.com/a/144317/26350
  * Description: A Wordpress Plugin to allow you to run Jetpack for Wordpress behind a firewall or on a local development site where remote activation is not possible.
+ * Author: Cody Mays
+ * Author URI: http://www.codymays.net
  * Version:     1.0.0
  */
-add_filter( 'jetpack_development_mode', '__return_true' );
+
+if(in_array( 'jetpack/jetpack.php', get_option( 'active_plugins', array() ) ))
+{
+        add_filter( 'jetpack_development_mode', '__return_true' );
+}
 
